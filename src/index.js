59b2d40343c3fbd9ca0f05f96e76d3e3cfdb0caa
JADundee <Jadundee@icloud.com>
2023-10-22 07:56:48 -0600

    Added card components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Card from './Card';
+import CardList from './CardList';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons'
+import {robots} from './robots';
 
 
 
@@ -19,12 +20,8 @@ import 'tachyons'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <CardList robots={robots}/>
   
-   <div>
-    <Card />
-    <Card />
-    <Card />
-   </div>
    
   
 );
