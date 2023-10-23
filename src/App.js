3c9e0740bc1a229ea7f49b38160ddca8498b10ea
JADundee <Jadundee@icloud.com>
2023-10-23 05:33:00 -0600

    Added API call and scroll component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, {Component} from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
 import './App.css';
+import Scroll from './Scroll'
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users').then(response =>  response.json())
+        .then(users => this.setState({robots: users}))
     }
 
     onSearch = (e) => {
@@ -21,13 +26,19 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
-        return (
-            <div className="tc">
-                <h1 className="f1">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearch}/>
-                <CardList robots={filteredRobots}/>
-            </div>
-        )
+        if (this.state.robots.length === 0) {
+            return <h1>Loading...</h1>
+        } else {
+            return (
+                <div className="tc">
+                    <h1 className="f1">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearch}/>
+                    <Scroll>
+                    <CardList robots={filteredRobots}/>
+                    </Scroll>
+                </div>
+            )
+        } 
     }
 }
 
